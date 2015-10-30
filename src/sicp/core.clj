66d5ded4fe833c1bbc327af5c50d0cba476dad4f
Rob Phoenix <robertphoenix@gmail.com>
2015-10-30 14:59:17 +0000

    completed exercises 1.2 & 1.3

--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -3,7 +3,7 @@
 ;;; Exercise 1.1
 ;;; Below is a sequence of expressions.
 ;;; What is the result printed by the interpreter in response to each expression?
-;;; Assume that the sequence is to be evaluated in the order in which it is presented. 
+;;; Assume that the sequence is to be evaluated in the order in which it is presented.
 
 10 ;; 10
 (+ 5 3 4) ;; 12
@@ -23,5 +23,20 @@
 (+ 2 (if (> b a) b a)) ;; 6
 (* (cond (> a b) a
          (< a b) b
-         :else -1)
+         "else" -1)
    (+ a 1)) ;; 16
+
+
+;;; Exercise 1.2
+;;; Translate the following expression into prefix form
+(/ (+ 4 5 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (- 6 2) (- 2 7))) ;; -37/150
+
+
+;;; Exercise 1.3
+;;; Define a function that takes three numbers as arguments
+;;; and returns the sum of the squares of the two larger numbers.
+(defn sum-sq-lrgst [x y z]
+  (reduce + (map #(* % %)
+                 [(first (reverse (sort [x y z])))
+                  (first (rest (reverse (sort [x y z]))))])))
